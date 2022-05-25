14cd5a3fb1c304ee0718df95c507b40926d7c722
Hoang Hiep <hoanghiep.hui@gmail.com>
2022-05-25 13:14:19 +0700

    up
    Signed-off-by: Hoang Hiep <hoanghiep.hui@gmail.com>

--- services/src/test/java/com/twidere/services/HttpConfigClientFactoryTest.kt
+++ services/src/test/java/com/twidere/services/HttpConfigClientFactoryTest.kt
@@ -1,22 +1,22 @@
 /*
- *  Twidere X
+ *  Twitter
  *
- *  Copyright (C) TwidereProject and Contributors
+ *  Copyright (C) 2021-2022 Living Solutions <living.solutions.vn@gmail.com>
  * 
- *  This file is part of Twidere X.
+ *  This file is part of Twitter.
  * 
- *  Twidere X is free software: you can redistribute it and/or modify
+ *  Twitter is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  * 
- *  Twidere X is distributed in the hope that it will be useful,
+ *  Twitter is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  * 
  *  You should have received a copy of the GNU General Public License
- *  along with Twidere X. If not, see <http://www.gnu.org/licenses/>.
+ *  along with Twitter. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.twidere.services
 
